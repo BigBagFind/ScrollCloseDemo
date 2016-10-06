761b21de90cc316d0cf60b4b2654ae4a0e5724fd
wuyt <594228924@qq.com>
2016-10-06 14:02:41 +0800

    Initial Commit

---
+++ ScrollCloseDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollCloseDemo
+//
+//  Created by 吴玉铁 on 2016/10/6.
+//  Copyright © 2016年 吴玉铁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollCloseDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollCloseDemo
+//
+//  Created by 吴玉铁 on 2016/10/6.
+//  Copyright © 2016年 吴玉铁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
